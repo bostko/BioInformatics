4b22d4843af86be21dd94168124716e1d947cfbd
Valentin Aitken <valentin@nalisbg.com>
2020-05-25 16:30:38 +0300

    Flask server

--- Valentin Aitken/SEMESTER_2/homework3-cache/homework3-cache-and-store-ncbi.py
+++ Valentin Aitken/SEMESTER_2/homework3-cache/homework3-cache-and-store-ncbi.py
@@ -1,51 +1,46 @@
-import redis
-import web
+from redis import Redis
+from pymongo import MongoClient
+from http.client import HTTPSConnection
+from flask import Flask, request, Response
+import urllib.parse
 
-urls = (
-    '/', 'Index'
-)
-
-
-class Index:
-    def GET(self):
-        return "Hello, world!"
-
-class NcbiQuery:
-    def __index__(self, query):
-        self.parse(query)
-
-    def parse(self, query):
-        pass
+EUTILS_KEY = 'eutils_keys'
 
 
 class NcbiCache:
-    def __init__(self, memory_cache_client, mongo_client):
+    def __init__(self, memory_cache_client: Redis, mongo_client: MongoClient):
         self.memory_cache_client = memory_cache_client
         self.mongo = mongo_client
+        self.http_conn = HTTPSConnection('eutils.ncbi.nlm.nih.gov')
+        self.http_conn.connect()
 
-    def get_url(self, url):
-        m_data = self.get_memory(url)
+    def get_eutils_url(self, url) -> str:
+        m_data = self.memory_cache_client.hget(EUTILS_KEY, url)
         if m_data:
-            return m_data.decode('utf-8')
-
-        db_data = self.get_db()
-
-    def get_memory(self, url):
-        return self.memory_cache_client.get(url)
-
-    def get_pubmed(self, id):
-        pass
-
-
-def main():
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    r.set('foo', 'bar')
-    r.hset('summary')
-    r.hmset()
-    print(r.get('foo'))
-    app = web.application(urls, globals())
-    app.run()
+            body = m_data.decode('utf-8')
+        else:
+            self.http_conn.request('GET', '/entrez/eutils/' + url)
+            response = self.http_conn.getresponse()
+            body = response.read()
+            self.memory_cache_client.hset(EUTILS_KEY, url, body)
+        response = Response(body)
+        response.headers['Content-Type'] = 'text/xml; charset=UTF-8'
+        return response
 
 
-if __name__ == "__main__":
-    main()
+rclient = Redis(host='localhost', port=6379, db=0)
+mongo_client = MongoClient("mongodb://localhost:27017/")
+ncbi_cache = NcbiCache(rclient, mongo_client)
+app = Flask(__name__)
+
+
+@app.route('/eutils', methods=['GET'])
+def eutils():
+    raw_query = request.args.get('query')
+    print(raw_query)
+    return ncbi_cache.get_eutils_url(raw_query)
+    # query = urllib.parse.quote(raw_query) if (' ' in raw_query) else raw_query
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=False)
